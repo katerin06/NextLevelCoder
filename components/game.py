d24c68a7a87708324221447e06ac3e00f712dc1e
katerin06 <katerinninaja@gmail.com>
2020-11-28 00:07:18 -0400

    Clase 3
    Clase 3

--- components/game.py
+++ components/game.py
@@ -1,31 +1,38 @@
 import pygame
+
+from components.player import  Player
 from utils.constants import (
      SCREEN_HEIGHT,
      SCREEN_WIDTH,
-     TITLE
+     TITLE,
+     BLACK
 )
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
         self.screen =pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
 
     def run(self):
         self.create_components()
         #Game loop:
         self.playing = True
         while self.playing:
+              self.clock.tick(120)
               self.events()
               self.update()
               self.draw()
         pygame.quit()
     def create_components(self):
-       pass
 
+        self.all_sprites = pygame.sprite.Group()
+        player = Player()
+        self.all_sprites.add(player)
 
 
     def update(self):
-       pass
+       self.all_sprites.update()
 
     def events(self):
         for event in pygame.event.get():
@@ -35,4 +42,7 @@ class Game:
 
 
     def draw(self):
-        pass
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+
